5c4717749b5891659c76452cfc43b358ef091b09
yarblade <yarblade@gmail.com>
2014-08-15 18:34:37 +0400

    Sources unit tests were added.

---
+++ SettingsReader.UnitTests/Sources/AppSettingsSourceTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SettingsReader.Sources;
+
+
+
+namespace SettingsReader.UnitTests.Sources
+{
+	[TestClass]
+	[ExcludeFromCodeCoverage]
+	public class AppSettingsSourceTests
+	{
+		[TestInitialize]
+		public void TestInit()
+		{
+			_settingsSource = new AppSettingsSource();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void GetWithNullableSourceNameTest()
+		{
+			_settingsSource.Get(null);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void GetWithEmptySourceNameTest()
+		{
+			_settingsSource.Get(string.Empty);
+		}
+
+		private AppSettingsSource _settingsSource;
+	}
+}
---
+++ SettingsReader.UnitTests/Sources/ConfigurationSectionSettingsSourceTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SettingsReader.Sources;
+
+
+
+namespace SettingsReader.UnitTests.Sources
+{
+	[TestClass]
+	[ExcludeFromCodeCoverage]
+	public class ConfigurationSectionSettingsSourceTests
+	{
+		[TestInitialize]
+		public void TestInit()
+		{
+			_settingsSource = new ConfigurationSectionSettingsSource();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void GetWithNullableSourceNameTest()
+		{
+			_settingsSource.Get(null);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void GetWithEmptySourceNameTest()
+		{
+			_settingsSource.Get(string.Empty);
+		}
+
+		private ConfigurationSectionSettingsSource _settingsSource;
+	}
+}
