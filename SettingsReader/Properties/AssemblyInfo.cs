b6fc441663b31fb50067f3104c10053ce4adc7de
Dmitry <yarblade@users.noreply.github.com>
2015-02-26 16:25:06 +0300

    Version was updated to 1.0.1.0.

--- SettingsReader/Properties/AssemblyInfo.cs
+++ SettingsReader/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: InternalsVisibleTo("SettingsReader.UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010021a8333476721e813d6c5e9d76dc86ed0e344af78274e69037dc18c672c4a6f2a3eb2e20e6afe583109e16b05be91db7c4b83e8182ee97b707af5c401754dcb98b758ecc77c59195d0ea92fb16eead375d922f4304e5ba70bacdb3e2648bc34eea0e8f49faaab7262517279e2e581d059ab48fe1bfb06950d788fbea7883dd8c")]
 [assembly: InternalsVisibleTo("SettingsReader.UnitTests.BlackBox, PublicKey=002400000480000094000000060200000024000052534131000400000100010021a8333476721e813d6c5e9d76dc86ed0e344af78274e69037dc18c672c4a6f2a3eb2e20e6afe583109e16b05be91db7c4b83e8182ee97b707af5c401754dcb98b758ecc77c59195d0ea92fb16eead375d922f4304e5ba70bacdb3e2648bc34eea0e8f49faaab7262517279e2e581d059ab48fe1bfb06950d788fbea7883dd8c")]
